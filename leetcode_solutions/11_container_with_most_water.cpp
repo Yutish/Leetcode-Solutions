069a69e572de381e6611dc84855c685eb5870015
Yutish Farma <yutishfarma21offi@gmail.com>
2021-05-08 19:17:18 +0530

    Add files via upload

--- leetcode_solutions/11_container_with_most_water.cpp
+++ leetcode_solutions/11_container_with_most_water.cpp
@@ -2,7 +2,17 @@
 
 // Solution
 
-
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int l=0,r=height.size()-1,area = 0;
+        while(l < r){
+            area=max(area,(r-l)*min(height[l],height[r]));
+            height[l]<height[r]?l++:r--;
+        }
+        return area;
+    }
+};
 
 //Problem Statement
 
